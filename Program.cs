62874c3ba4239276d0418463b599fbf5755860f8
FzZarezadeh <fatmhzhrazarzadh@gmail.com>
2025-12-06 11:10:13 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace mudarib3
+{
+    class Program
+    {
+        static void multiplier(int a)
+        {
+            if (a >= 3)
+            {
+                if (a % 3 == 0)
+                {
+                    Console.WriteLine(a);
+                    multiplier(a - 3);
+                }
+                else
+                    multiplier(a - 1);
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("end");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int n = int.Parse(Console.ReadLine());
+            multiplier(n-1);
+            Console.ReadKey();
+        }
+    }
+}
